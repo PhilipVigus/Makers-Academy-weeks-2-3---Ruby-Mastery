33203df72729e0924b4095818d21cdfcd7b9710d
PhilipVigus <philipvigus@yahoo.co.uk>
2020-02-21 08:09:21 +0000

    Implement a simple dictionary using a hash

---
+++ Chapter 8/simple_dictionary.rb
@@ -0,0 +1,11 @@
+#create the dictionary hash
+#add each definition one at a time, checking them as you go to make sure they work
+simple_dictionary = {
+  "bear" => "a creature that fishes in the river for salmon",
+  "river" => "a body of water that contains salmon, and sometimes bears",
+  "salmon" => "a fish, sometimes in a river, sometimes in a bear, and sometimes in both"
+}
+
+puts simple_dictionary["bear"]
+puts simple_dictionary["river"]
+puts simple_dictionary["salmon"]
